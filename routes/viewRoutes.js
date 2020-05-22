1f138c35554d3e2ec981c1263babe6ccd970fc95
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-05-21 23:12:13 -0700

    create account page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
-router.use(authController.isLoggedIn);
+// router.use();
 
-router.get('/', viewsController.getOverview);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
-router.get('/tour/:slug', viewsController.getTour);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
-router.get('/login', viewsController.getLoginForm);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
